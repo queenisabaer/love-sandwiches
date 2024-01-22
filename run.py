43aaaa040c6ce3718c1aea755fd2a6b225651381
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-01-22 09:48:31 +0100

    Add validation to check for exactly  6 values in provided data

--- run.py
+++ run.py
@@ -18,9 +18,31 @@ def get_sales_data():
     """
     print("Please enter sales data from the last market.")
     print("Data should ne six numbers, separated by comma.")
-    print("Example: 10,20,30,40,50,60\n")
+    print("Example: 10,20,30,40,50,60\n") #to add a new line between example and data input field \n
 
     data_str = input("Enter your data her: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",") #split() method returns the broken up values as a list and removes commas from the string. convert string value into a list of values, each separated by a comma
+    validate_data(sales_data) 
+
+
+#validate data before allowing the rest of the programm to continue
+def validate_data(values): #parameter of 'values' which will be our sales data list 
+    """
+    Inside tge try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values
+    """
+    #try-except statement to check if data is valid
+    try: #code that runs with no errors if data is valid
+        if len(values) != 6: #raise a ValueError if lenght of data is not 6
+            #len()method returns the length of the list - the number of values in it
+            raise ValueError(#create own costum error message
+                f"Exactly 6 calues required, you provided {len(values)}" 
+            )
+    except ValueError as e: #print an error to the terminal, Value Error class containt the details of the error trigerd by the code in try statement
+         #by using as we're assigning that ValueError object to the e variable(standard for error)
+         print(f"Invalid data: {e}, please try again.\n ")#print error if try fails
+
 
 get_sales_data()
